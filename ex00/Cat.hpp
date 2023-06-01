59967f788393f6ec8da2f0d72198c11d7320575a
Pierre SZLEPER <psz@bs.com>
2023-06-01 07:27:56 +0200

    Added virtual destructors in ex00

--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -6,7 +6,7 @@
 /*   By: pszleper < pszleper@student.42.fr >        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 23:13:01 by pszleper          #+#    #+#             */
-/*   Updated: 2023/05/30 15:48:46 by pszleper         ###   ########.fr       */
+/*   Updated: 2023/06/01 07:23:44 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class Cat : public Animal
 			Cat(void);								// canonical
 			Cat(const Cat& cat_to_copy);		// canonical
 	Cat&	operator=(const Cat& cat_to_copy);	// canonical
-			~Cat(void);								// canonical
+			virtual ~Cat(void);								// canonical
 
 			virtual void	makeSound(void) const;
 };
